0023913a926727512ccd873f9dc5a112cdf953ab
jason-bowen-zheng <13239073174@wo.cn>
2020-07-25 14:51:22 +0800

    2020-07-25

--- funclib.py
+++ funclib.py
@@ -1,2 +1,19 @@
 # func/funclib.py
 # func runtime lib
+
+import sys
+
+class core(object):
+
+    def __init__(self):
+        self.function = {}
+
+    def run(self):
+        while True:
+            try:
+                cmd = input('> ')
+            except KeyboardInterrupt:
+                print()
+                sys.exit(1)
+            else:
+                pass
